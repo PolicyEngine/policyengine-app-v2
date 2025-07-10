e45c7be679feed2a1c219f19df5979aa660f1e5f
Anthony Volk <anth.volk@gmail.com>
2025-07-10 13:21:03 -0400

    feat: Add logging to demonstrate functionality

--- src/modals/PolicyCreationModal.tsx
+++ src/modals/PolicyCreationModal.tsx
@@ -7,19 +7,27 @@ import { useState } from "react";
 export default function PolicyCreationModal() {
   const dispatch = useDispatch();
 
-  const [localPolicyLabel, setLocalPolicyLabel] = useState("");
+  // Manage instantaneous changes to the label input
+  // locally, then emit the final to the reducer to avoid
+  // visual lag in reducer updates
+  const [localLabel, setLocalLabel] = useState("");
+
+  function handleLocalLabelChange(value: string) {
+    console.log("Updating local label:", value);
+    setLocalLabel(value);
+  }
 
   const formInputs = (
     <TextInput
       label="Reform title"
       placeholder="Policy name"
-      value={localPolicyLabel}
-      onChange={(e) => setLocalPolicyLabel(e.currentTarget.value)}
+      value={localLabel}
+      onChange={(e) => handleLocalLabelChange(e.currentTarget.value)}
     />
   )
 
   function submissionHandler() {
-    dispatch(updateLabel(localPolicyLabel));
+    dispatch(updateLabel(localLabel));
   }
 
   return (
