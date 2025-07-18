84d126d46b190bb0c239ed5f085e2b0a2e07d0be
SakshiKekre <sakshi.kekre@gmail.com>
2025-07-18 10:05:31 -0700

    added tanstack devtools

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Provider } from 'react-redux';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
@@ -16,6 +17,7 @@ export default function App() {
       <MantineProvider theme={theme}>
         <QueryClientProvider client={queryClient}>
           <Router />
+          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </MantineProvider>
     </Provider>
