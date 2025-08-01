309de747cf7c4327fcd2ad4835ce819f2e4500df
Anthony Volk <anth.volk@gmail.com>
2025-08-01 18:08:21 -0300

    fix: Move UserSimulation to different location

--- src/types/simulation.ts
+++ src/types/simulation.ts
@@ -2,9 +2,3 @@ export interface Simulation {
   populationId: string;
   policyId: string;
 }
-
-export interface UserSimulation {
-  userId: string;
-  simulationId: string;
-  label?: string;
-}
--- src/types/userIngredientAssociations.ts
+++ src/types/userIngredientAssociations.ts
@@ -4,3 +4,9 @@ export interface UserPolicyAssociation {
   createdAt?: string;
   updatedAt?: string;
 }
+
+export interface UserSimulation {
+  userId: string; // TODO: Verify type; see if we want to integrate with UserPolicy or Policy
+  simulationId: string; // TODO: Verify type; see if we want to integrate with UserPolicy or Policy
+  label?: string;
+}
