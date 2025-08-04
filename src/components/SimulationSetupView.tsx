d621b0409dea7552e3dc7e4cbb1b96e97e6010a5
Anthony Volk <anth.volk@gmail.com>
2025-08-04 20:30:52 -0300

    feat: Properly display policy in sim flow if policy is already loaded

--- src/components/SimulationSetupView.tsx
+++ src/components/SimulationSetupView.tsx
@@ -1,7 +1,8 @@
 import SimulationSetupPolicyFrame from '@/frames/SimulationSetupPolicyFrame';
 import SimulationSetupPopulationFrame from '@/frames/SimulationSetupPopulationFrame';
 import { Button, Container, Grid, Stack, Text, Card } from '@mantine/core';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 interface SimulationSetupViewProps {
   onPopulationSelect?: () => void;
@@ -26,6 +27,12 @@ export default function SimulationSetupView({
   // TODO: Handle navigation/display after a user goes through policy creation flow;
   // policies not yet actually selecting after creation
 
+  const userDefinedPolicy = useSelector((state: any) => state.policy);
+
+  useEffect(() => {
+    console.log('User defined policy updated:', userDefinedPolicy);
+  }, [userDefinedPolicy]);
+
   return (
     <Container size="md" py="xl">
       <Stack>
@@ -36,14 +43,45 @@ export default function SimulationSetupView({
             Select a geographic scope or specific household
           </Text>
         </Card>
-        <Card withBorder p="md" mb="xl" component="button" onClick={onPolicySelect}>
-          <Text fw={700}>TODO: ICON</Text>
-          <Text>Add policy</Text>
-          <Text size="sm" c="dimmed">
-            Select a policy to apply to the simulation
-          </Text>
-        </Card>
+        {/* TODO: Add pointer cursor on hover over card*/}
+        {userDefinedPolicy && userDefinedPolicy.isCreated ? (
+          <CardSelectedPolicy label={userDefinedPolicy.label} />
+        ) : (
+          <CardCreatePolicy onClick={onPolicySelect} />
+        )}
       </Stack>
     </Container>
+  );
+}
+
+interface CardSelectedPolicyProps {
+  label: string;
+}
+
+interface CardCreatePolicyProps {
+  onClick: () => void;
+}
+
+function CardSelectedPolicy({ label }: CardSelectedPolicyProps) {
+  return (
+    <Card withBorder p="md" mb="xl" component="button">
+      <Text fw={700}>TODO: ICON</Text>
+      <Text>{label}</Text>
+      <Text size="sm" c="dimmed">
+        TODO: Policy description and details
+      </Text>
+    </Card>
+  );
+}
+
+function CardCreatePolicy({ onClick }: CardCreatePolicyProps) {
+  return (
+    <Card withBorder p="md" mb="xl" component="button" onClick={onClick}>
+      <Text fw={700}>TODO: ICON</Text>
+      <Text>Add policy</Text>
+      <Text size="sm" c="dimmed">
+        Select a policy to apply to the simulation
+      </Text>
+    </Card>
   );
 }
