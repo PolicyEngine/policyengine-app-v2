eef60caeab2824bedb9fc3ba5054e7fa575c7b44
Anthony Volk <anth.volk@gmail.com>
2025-08-07 14:58:06 -0300

    chore: Typecheck

--- src/frames/SimulationSetupPopulationFrame.tsx
+++ src/frames/SimulationSetupPopulationFrame.tsx
@@ -1,7 +1,13 @@
 import NewExistingIngredientSelector from "@/components/NewExistingIngredientSelector";
 
 export default function SimulationSetupPopulationFrame() {
+
+  // TODO: After roping in population ingredient, write correct functions
+  function temporaryNullFunc() {
+    return;
+  }
+
   return (
-    <NewExistingIngredientSelector ingredientName="population" />
+    <NewExistingIngredientSelector ingredientName="population" onClickCreateNew={temporaryNullFunc} onClickExisting={temporaryNullFunc}/>
   );
 }
