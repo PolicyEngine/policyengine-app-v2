6b40fc8c20e79a6fd8e1a7ab0306ee3be7229c82
Jivika Gulrajani <jivi@Jivikas-MacBook-Air.local>
2025-08-21 02:29:40 -0700

    feat: add generic Embed component for AI demo page (#110)

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home.page';
+import AIDemoPage from './pages/AIDemo.page';
+
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/us/ai',
+    element: <AIDemoPage />,
   },
 ]);
 
