64fa56c602a36c9f6ffd66c318258caea62b1be4
Anthony Volk <anth.volk@gmail.com>
2025-09-03 14:25:03 -0200

    feat: Use new reducer in SimulationCreationFrame

--- app/src/frames/SimulationCreationFrame.tsx
+++ app/src/frames/SimulationCreationFrame.tsx
@@ -1,20 +1,44 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { TextInput } from '@mantine/core';
 import FlowView from '@/components/common/FlowView';
 import { updateSimulationLabel } from '@/reducers/simulationReducer';
+import { 
+  createSimulation, 
+  updateSimulationLabel as updateSimulationsLabel 
+} from '@/reducers/simulationsReducer';
+import { RootState } from '@/store';
 import { FlowComponentProps } from '@/types/flow';
 
 export default function SimulationCreationFrame({ onNavigate }: FlowComponentProps) {
   const [localLabel, setLocalLabel] = useState('');
   const dispatch = useDispatch();
+  
+  // Get the active simulation ID from the new reducer (if it exists)
+  const activeSimulationId = useSelector((state: RootState) => state.simulations?.activeId);
+
+  // TODO: Make sure this effect hook is compatible with how we want to set up ingredients
+  useEffect(() => {
+    // If there's no active simulation in the new reducer, create one
+    // This ensures we have a simulation to work with in the new structure
+    if (!activeSimulationId) {
+      dispatch(createSimulation());
+    }
+  }, [activeSimulationId, dispatch]);
 
   function handleLocalLabelChange(value: string) {
     setLocalLabel(value);
   }
 
   function submissionHandler() {
+    // Dispatch to old reducer (for backward compatibility)
     dispatch(updateSimulationLabel(localLabel));
+    
+    // Dispatch to new reducer (for forward compatibility)
+    if (activeSimulationId) {
+      dispatch(updateSimulationsLabel({ label: localLabel }));
+    }
+    
     onNavigate('next');
   }
 
