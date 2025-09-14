fd0efe14c842dceae4d0dba48330cb5a900dcc0d
Anthony Volk <anth.volk@gmail.com>
2025-09-14 17:39:34 +0100

    fix: Move society-wide output files to correct folder

--- types/metadata/reportOutputMetadata/ReportOutputSocietyWideByConstituency.ts
+++
@@ -1,8 +0,0 @@
-export interface ReportOutputSocietyWideByConstituency {
-  [constituencyName: string]: {
-    average_household_income_change: number;
-    relative_household_income_change: number;
-    x: number;
-    y: number;
-  };
-}
