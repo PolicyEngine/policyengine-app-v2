ec3d3f8aa9901da0a408e70b37ac70883ed221e5
Anthony Volk <anth.volk@gmail.com>
2025-09-25 15:57:32 +0100

    feat: CalculationManager

---
+++ app/src/libs/calculations/index.ts
@@ -0,0 +1,27 @@
+// Type utilities
+export {
+  type CalculationType,
+  determineCalculationType,
+  extractPopulationId,
+  extractRegion
+} from './types';
+
+// Status types
+export {
+  type CalculationStatus,
+  type CalculationStatusResponse
+} from './status';
+
+// Handlers
+export {
+  CalculationHandler,
+  EconomyCalculationHandler,
+  HouseholdCalculationHandler
+} from './handlers';
+
+// Manager
+export {
+  CalculationManager,
+  getCalculationManager,
+  resetCalculationManager
+} from './manager';
