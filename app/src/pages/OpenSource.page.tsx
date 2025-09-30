fb08560e7d1869d5ce10e887e03a73f705b117bc
harnish7576 <savadiaharnish@gmail.com>
2025-09-30 12:32:22 -0400

    initial commit

---
+++ app/src/pages/OpenSource.page.tsx
@@ -0,0 +1,39 @@
+import {Text} from '@mantine/core';
+import PageHeader from '@/components/shared/static/PageHeader';
+import { TitleCardWithHeader } from '@/components/shared/static/TextCardWithHeader';
+
+export default function OpenSourcePage() {
+  return (
+    <>
+      <PageHeader
+        title="Open source"
+        description="PolicyEngine's commitment to open source software and transparent policy analysis"
+      />
+
+{/*       <TitleCardWithHeader
+        title="Coming soon"
+        sections={[
+          {
+            body: (
+              <>
+                <Text>
+                  We're currently developing comprehensive content about PolicyEngine's open source
+                  software and our commitment to transparency. This page will include:
+                </Text>
+
+                <ul style={{ marginTop: '1rem', paddingLeft: '1.5rem' }}>
+                  <li>Overview of our open source repositories and software architecture</li>
+                  <li>Contributor guidelines and how to get involved</li>
+                  <li>Our open source philosophy and commitment to transparency</li>
+                  <li>Case studies of community contributions</li>
+                  <li>Development roadmap and future plans</li>
+                </ul>
+              </>
+            ),
+          },
+        ]}
+      /> */}
+
+    </>
+  );
+}
