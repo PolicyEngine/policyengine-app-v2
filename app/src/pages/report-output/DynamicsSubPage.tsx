c784da5593cbf15624ef70ef7c4bc022b3a9dd11
Anthony Volk <anth.volk@gmail.com>
2025-10-16 02:08:58 +0200

    feat: DynamicsSubPage stub, test fixes and utils

---
+++ app/src/pages/report-output/DynamicsSubPage.tsx
@@ -0,0 +1,9 @@
+/**
+ * DynamicsSubPage - Placeholder for dynamics visualization
+ *
+ * This component will eventually show dynamic analysis of policy impacts over time.
+ * Currently displays a placeholder message.
+ */
+export default function DynamicsSubPage() {
+  return <div>Dynamics</div>;
+}
