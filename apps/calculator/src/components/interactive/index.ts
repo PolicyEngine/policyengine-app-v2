bbec827512b9559b63bc108d724dd6fb45c9a866
SakshiKekre <sakshi.kekre@gmail.com>
2025-12-26 11:08:30 -0800

    chore: Remove unused website-only code from calculator

--- apps/calculator/src/components/interactive/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Interactive Components
- *
- * Components for embedding external interactive apps and calculators.
- */
-
-export { default as StreamlitEmbed } from './StreamlitEmbed';
-export { default as OBBBAIframeContent } from './OBBBAIframeContent';
